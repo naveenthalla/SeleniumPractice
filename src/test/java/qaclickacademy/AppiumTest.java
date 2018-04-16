0d78d754cc24e36d68858ba9148dbc702eebc634
naveenthalla <naveengoud999@gmail.com>
2018-04-16 11:57:02 +0100

    First commit for Maven Java project

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeAndroidApp()
+	{
+		
+		System.out.println("nativeAndroidApp");
+	}
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementsui()
+	{
+		System.out.println("elementsui");
+		
+	}
+	
+
+}
